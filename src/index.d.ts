d339e6e8addf101526e7a6f513cc656144aea43a
GwunGG <dima_osipov_2000@bk.ru>
2022-04-04 20:33:22 +0500

    Задание по типам данных TS

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,13 @@
 - age - число
 - gender - 'male' или 'female'
  */
-export function getPersons();
+type GetPerson = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+};
+type GetPersons = GetPerson[];
+export function getPersons(): GetPersons;
 
 /*
 Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
@@ -18,4 +24,13 @@ export function getPersons();
    - gender - 'male' или 'female'
    - company- строка
  */
-export function personToString(person);
+interface Person {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+}
+interface Worker extends Person {
+    company: string;
+}
+
+export function personToString(person: Person | Worker): string;
